a9c9b57107b1ce90de7b5cae48ba5dbe7d14a700
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2024-09-19 11:36:17 +0100

    build free standing binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // exclude rust standard library as it requires existing os features
+#![no_main] // disable rust level entry points
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle] // don't mangle the name of this function as most linkers expect the entry point name to be '_start'
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
