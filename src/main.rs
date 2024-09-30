89f54249c7c37dcf14730120d1996f82c0643810
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2024-09-30 20:55:36 +0100

    Implement vga_buffer module to handle writing to vga_buffer and implement println! macro and use it in panic

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
 #![no_std] // exclude rust standard library as it requires existing os features
 #![no_main] // disable rust level entry points
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello world!";
-
 #[no_mangle] // don't mangle the name of this function as most linkers expect the entry point name to be '_start'
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
