4efa750a54681abd0a58f546241a061771c46e19
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2024-10-01 11:23:07 +0100

    implement unit tests and integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
 #![no_std] // exclude rust standard library as it requires existing os features
 #![no_main] // disable rust level entry points
-
-mod vga_buffer;
+#![feature(custom_test_frameworks)]
+#![test_runner(hobbyos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use hobbyos::println;
 
+/// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    hobbyos::test_panic_handler(info)
+}
+
 #[no_mangle] // don't mangle the name of this function as most linkers expect the entry point name to be '_start'
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
