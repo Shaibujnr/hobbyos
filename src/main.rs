722732f14bb616a79080b72a02bb5dbbb2da8b1a
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2024-10-02 23:06:33 +0100

    implement double fault handler and use IST to run double fault handler on a separate stack

--- src/main.rs
+++ src/main.rs
@@ -27,9 +27,6 @@ pub extern "C" fn _start() -> ! {
 
     hobbyos::init();
 
-    //invoke a break point exception
-    x86_64::instructions::interrupts::int3();
-
     #[cfg(test)]
     test_main();
 
