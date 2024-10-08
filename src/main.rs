bcc985b4a207a9d43c5d61fc59e7904950d8cc98
Shaibu Shaibu <s.shaibu.jnr@gmail.com>
2024-10-08 10:24:07 +0100

    implement heap allocation using linked list allocator

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@
 #![test_runner(hobbyos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
-use hobbyos::{
-    memory::{translate_addr, BootInfoFrameAllocator},
-    println,
-};
+use hobbyos::{memory::BootInfoFrameAllocator, println};
 
 entry_point!(kernel_main);
 
@@ -28,6 +28,7 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use hobbyos::allocator;
     use hobbyos::memory;
     use x86_64::{
         structures::paging::{Page, Translate},
@@ -41,13 +42,28 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    // map an unused page
-    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    // write the string `New!` to the screen through the new mapping
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+    let heap_value = Box::new(41);
+    println!("heap value at {:p}", heap_value);
+
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     #[cfg(test)]
     test_main();
